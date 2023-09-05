9cf7b0ecae0797a16f48826fb7ec858f2d16afec
sanju <sanjeevhindouliya@gmail.com>
2023-09-05 08:30:16 -0700

    api config

---
+++ src/main/java/com/sanjeev/rest/GreetRestController.java
@@ -0,0 +1,20 @@
+package com.sanjeev.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+	
+	@Value("${msg}")
+	private String msg;
+	
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+		return msg;
+		
+	}
+	
+
+}
